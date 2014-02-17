5fdb79ad6faa9b6a913592568260a230f0856d75
Micah Wylde <micah@micahw.com>
2014-02-17 00:47:46 -0800

    Stuff

--- src/main/scala/com/micahw/threes/Game.scala
+++ src/main/scala/com/micahw/threes/Game.scala
@@ -2,26 +2,33 @@ package com.micahw.threes
 
 sealed abstract class Tile
 
+case object Blank extends Tile
 case object OneTile extends Tile
 case object TwoTile extends Tile
 case class ThreeTile(power : Int) extends Tile
 
 object Tile {
   def value(t : Tile) = t match {
+    case Blank => 0
     case OneTile => 1
     case TwoTile => 2
-    case ThreeTile(power) => 3 * (2 << power)
+    case ThreeTile(power) => 3 * (1 << power)
   }
 }
 
 class Board(val board : List[List[Tile]]) {
-
   override def toString = {
-    ""
+    board.map { row =>
+      row.map(Tile.value).map(_.toString).mkString("\t")
+    }.mkString("\t")
   }
 }
 
-object Board
+object Board {
+  def random = {
+    val pieces = List(Blank)
+  }
+}
 
 class Game {
 
